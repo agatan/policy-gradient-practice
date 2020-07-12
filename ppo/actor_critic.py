c16656c0236775b3f410bd32eecc242dc65d9137
agatan <agatan039@gmail.com>
2020-07-12 15:50:50 +0900

    Implement ActorCritic

--- ppo/actor_critic.py
+++ ppo/actor_critic.py
@@ -3,10 +3,11 @@ from typing import Callable, List, Optional, Sequence, Tuple
 
 import numpy as np
 import torch
-from torch import dist, distributions
+from torch import distributions
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributions as distributions
+from gym import spaces
 
 
 Observation = torch.Tensor
@@ -60,3 +61,58 @@ class MLPCategoricalActor(Actor):
         self, pi: distributions.Distribution, act: torch.Tensor
     ) -> torch.Tensor:
         return pi.log_prob(act)
+
+
+class MLPCritic(nn.Module):
+    def __init__(
+        self, obs_dim: int, hidden_sizes: Sequence[int], activation=F.relu
+    ) -> None:
+        super().__init__()
+        self.v_net = _mlp([obs_dim] + list(hidden_sizes) + [1], activation)
+
+    def forward(self, obs: Observation) -> torch.Tensor:
+        """Returns estimated values of observations.
+
+        Args:
+            obs (Observation): Observed state. (batch_size, obs_dim)
+
+        Returns:
+            torch.Tensor: estimated values. (batch_size,)
+        """
+        return self.v_net(obs).squeeze(1)
+
+
+class MLPActorCritic(nn.Module):
+    def __init__(
+        self,
+        obs_dim: int,
+        action_space: spaces.Space,
+        hidden_sizes=(64, 64),
+        activation=nn.Tanh,
+    ) -> None:
+        super().__init__()
+        assert isinstance(action_space, spaces.Discrete)
+
+        self.pi = MLPCategoricalActor(obs_dim, action_space.n, hidden_sizes, activation)
+        self.v = MLPCritic(obs_dim, hidden_sizes, activation)
+
+    @torch.no_grad
+    def step(self, obs: Observation) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """Step
+
+        Args:
+            obs (Observation): [description]
+
+        Returns:
+            actions (np.ndarray): selected actions.
+            values (np.ndarray): estimated values of the observation.
+            logp_a (np.ndarray): log probability of the selected actions.
+        """
+        pi = self.pi._distribution(obs)
+        a = pi.sample()
+        logp_a = self.pi._log_prob_from_distribution(pi, a)
+        v = self.v(obs)
+        return a.numpy(), v.numpy(), logp_a.numpy()
+
+    def act(self, obs: Observation) -> np.ndarray:
+        return self.step(obs)[0]
